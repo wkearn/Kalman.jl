09e286cd19d778405cd7b7a2190512ff20b31926
Will Kearney <wkearn@gmail.com>
2014-09-13 21:28:32 -0400

    Add type construction tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using Kalman
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@test super(BasicKalmanFilter) == LinearKalmanFilter
+
+s = State([1.,0],[0.5 0; 0 1e-10])
+
+@test typeof(s) == State{Float64}
+@test s.x == [1,0]
+@test s.p == [0.5 0; 0 1e-10]
+
+o = Observation([2.],[0.5]')
+
+@test typeof(o) == Observation{Float64}
+@test o.y == [2]
+@test o.r == [0.5]'
