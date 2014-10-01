3ce6d07b411847856c877d7c4ffad40d11ebfea1
Will Kearney <wkearn@gmail.com>
2014-10-01 09:10:29 -0400

    Move sigma functions over to unscentedtypes.jl Add outer constructor for filter

--- src/unscentedtypes.jl
+++ src/unscentedtypes.jl
@@ -22,9 +22,38 @@ type AdditiveUnscentedKalmanFilter <: UnscentedKalmanFilter
     κ::Real
     wm::Vector
     wc::Vector
-    function AdditiveUnscentedKalmanFilter(x::State,f::AdditiveUnscentedModel,z::AdditiveUnscentedObservationModel,α::Real,β::Real,κ::Real)
+end
+
+function AdditiveUnscentedKalmanFilter(x::State,f::AdditiveUnscentedModel,z::AdditiveUnscentedObservationModel,α::Real,β::Real,κ::Real)
         σ = sigma(x,α,κ)
         (wm,wc) = sigmaweights(length(x.x),α,β,κ)
-        new(x,f,z,σ,α,β,κ,wm,wc)
+        AdditiveUnscentedKalmanFilter(x,f,z,σ,α,β,κ,wm,wc)
+end
+
+function sigma(x::Vector,p::Matrix,α,κ)
+    n = length(x)
+    σ = fill(zeros(x),2n+1)
+    γ = sqrt(n+α^2*(n+κ)-n)
+    σ[1] = x
+    sp = γ*chol(p)
+    for i = 2:n+1
+        σ[i] = x + sp[:,i-1]
+        σ[i+n] = x - sp[:,i-1]
     end
+    σ
+end
+
+sigma(x::State,α,κ) = sigma(x.x,x.p,α,κ)
+
+function sigmaweights(n,α,β,κ)
+    wm = zeros(2n+1)
+    wc = zeros(2n+1)
+    λ = α^2*(n+κ)-n
+    wm[1] = λ/(n+λ)
+    wc[1] = wm[1] + (1-α^2+β)
+    for i = 2:2n+1
+        wm[i] = 1/(2*(n+λ))
+        wc[i] = wm[i]
+    end
+    (wm,wc)
 end
