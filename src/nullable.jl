5ef9f174107735d26b066e8a5569b747ae2aa7b2
Will Kearney <wkearn@gmail.com>
2014-10-23 11:21:39 -0400

    Abstract the application of Nullables so that it applies to all filter implementations

--- src/nullable.jl
+++ src/nullable.jl
@@ -1,28 +1,18 @@
 # Filters using Nullable types to represent missing measurements
 
-function update{T}(kf::BasicKalmanFilter,y::Nullable{Vector{T}})
+function update{T}(kf::KalmanFilter,y::Nullable{Vector{T}})
     if isnull(y.y)
         return kf
     else
-        res = get(y) - kf.z.h * kf.x.x
-        s = kf.z.h * kf.x.p*kf.z.h' + kf.z.r
-        k = kf.x.p * kf.z.h' * inv(s)
-        xn = kf.x.x + k*res
-        pn = kf.x.p - k * kf.z.h * kf.x.p
-        return BasicKalmanFilter(State(xn,pn),kf.f,kf.z)
+        return update(kf,Observation(get(y)))
     end
 end
 
-function update!{T}(kf::BasicKalmanFilter,y::Nullable{Vector{T}})
+function update!{T}(kf::KalmanFilter,y::Nullable{Vector{T}})
     if isnull(y)
         return kf
     else
-        res = get(y) - kf.z.h * kf.x.x
-        s = kf.z.h * kf.x.p * kf.z.h' + kf.z.r
-        k = kf.x.p*kf.z.h' * inv(s)
-        xn = kf.x.x + k*res
-        pn = kf.x.p - k * kf.z.h * kf.x.p
-        kf.x = State(xn,pn)
+        update!(kf,Observation(get(y)))
         return kf
     end
 end
--- test/nullabletest.jl
+++ test/nullabletest.jl
@@ -1,4 +1,5 @@
 using Kalman, Base.Test
+using Kalman.EKF
 
 x0 = State([1.,0],[0.5 0; 0 1e-10])
 dt = 0.01
@@ -38,4 +39,61 @@ update!(kf1,Nullable{Vector{Float64}}())
 
 @test kf1.x != kf.x
 
+update!(kf1,Nullable([0.98]))
 
+@test kf1.x == kf.x
+
+## Extended Kalman Filter with Nullables
+
+k = 1.0
+p0 = 0.1*k
+r = 10.0
+dt = 0.0005
+
+logit(p0,k,x) = (k*p0*exp(x))/(k+p0*(exp(x)-1))
+
+function an(x,k,r,deltaT)
+    x1 = zeros(x)
+    x1[1] = x[1]
+    x1[2] = logit(x[2],k,r*deltaT)
+    return x1
+end
+
+fekf(x) = an(x,k,x[1],dt)
+gekf(x) = eye(2)
+hekf(x) = x[2]
+
+Q = [1e-10 0.0; 0.0 1e-10]
+R = [1e-2]'
+
+x0 = State([5.0,0.1],[1e2 0.0; 0.0 1e-10])
+
+ekff = NonlinearModel(fekf,gekf,Q)
+z = NonlinearObservationModel(hekf,R)
+
+kf0 = BasicExtendedKalmanFilter(x0,ekff,z)
+kf = kf0
+
+kf1 = deepcopy(kf)
+
+predict!(kf)
+predict!(kf1)
+
+@test kf1.x == kf.x
+
+update!(kf,Observation([0.1]))
+update!(kf1,Nullable([0.1]))
+
+@test kf1.x == kf.x
+
+predict!(kf)
+predict!(kf1)
+
+update!(kf,Observation([0.09]))
+update!(kf1,Nullable{Vector{Float64}}())
+
+@test kf1.x != kf.x
+
+update!(kf1,Nullable([0.09]))
+
+@test kf1.x == kf.x
