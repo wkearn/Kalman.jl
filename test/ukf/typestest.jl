0e3cdfaefa8dad21e9b4123b129e935917ce8244
Will Kearney <wkearn@gmail.com>
2014-10-26 15:03:19 -0400

    Fix typos which stopped tests

--- test/ukf/typestest.jl
+++ test/ukf/typestest.jl
@@ -10,19 +10,19 @@ s = UnscentedState([1.0, 0.0],4*eye(2),0.1,2.0,0.0)
 @test_approx_eq dot(s.wm,s.σ) s.x
 @test_approx_eq dot(s.wc,map(x->(x-s.x)*(x-s.x)',s.σ)) s.p
 
-const dt = 0.01
+dt = 0.01
 
-function f(x)
+function fu(x)
     x1 = zeros(x)
     x1[1] = x[1] + dt*x[2]
     x1[2] = x[2] - dt*x[1]
     x1
 end
 
-fm = AdditiveUnscentedModel(f,1e-10*eye(2))
+fm = AdditiveUnscentedModel(fu,1e-10*eye(2))
 
-h(x) = [x[1]]
+hu(x) = [x[1]]
 
-zm = AdditiveUnscentedObservationModel(h,[0.1]')
+zm = AdditiveUnscentedObservationModel(hu,[0.1]')
 
 kf = AdditiveUnscentedKalmanFilter(s,fm,zm)
