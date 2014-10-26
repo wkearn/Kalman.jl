d18b289e3b8132157ceecd9f852b796b95d35cff
Will Kearney <wkearn@gmail.com>
2014-10-26 13:28:28 -0400

    Add update tests to filtertest

--- test/ukf/filtertest.jl
+++ test/ukf/filtertest.jl
@@ -1,10 +1,19 @@
 include("typestest.jl")
 
 kf0 = copy(kf)
-kf1 = copy(kf)
 
+kf1 = predict(kf)
 predict!(kf)
-kf2 = predict(kf1)
+
+@test kf1.x.x == kf.x.x 
+@test_approx_eq kf.x.x [1.0,-0.01]
+@test kf1.x.p == kf.x.p
+@test kf1.x.σ == kf.x.σ
+
+y = Observation([1.01])
+
+kf2 = update(kf,y)
+update!(kf,y)
 
 @test kf2.x.x == kf.x.x
 @test kf2.x.p == kf.x.p
