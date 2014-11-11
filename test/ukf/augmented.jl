649409701796b192342255f3c5a63b77442a57b0
Will Kearney <wkearn@gmail.com>
2014-11-11 11:31:18 -0500

    Tests for the measurement update step

--- test/ukf/augmented.jl
+++ test/ukf/augmented.jl
@@ -24,4 +24,19 @@ end
 
 fm = AugmentedUnscentedModel(f,1e-10*eye(2))
 
-(xn,pn) = ap(fm,s)
+sn = ap(fm,s)
+
+@test_approx_eq_eps sn.x [1.0,-0.01] 2e-10
+@test_approx_eq_eps sn.p [4.0004 0; 0 4.0004] 2e-10
+
+function h(x)
+    x1 = zeros(1)
+    x1[1] = x[1] + x[3]
+end
+
+zm = AugmentedUnscentedObservationModel(h,[0.1]')
+y = Observation([1.01])
+
+kf = AugmentedUnscentedKalmanFilter(sn,fm,zm)
+
+(res,ph,s) = covs(kf,y)
