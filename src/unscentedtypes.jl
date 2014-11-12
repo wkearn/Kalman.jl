a57a41152913f0921d544d9621c0933e72c9444b
Will Kearney <wkearn@gmail.com>
2014-11-12 10:49:15 -0500

    Add matrix-based sigma method

--- src/unscentedtypes.jl
+++ src/unscentedtypes.jl
@@ -39,6 +39,25 @@ function AdditiveUnscentedKalmanFilter(x::State,f::AdditiveUnscentedModel,z::Add
     AdditiveUnscentedKalmanFilter(s,f,z)
 end
 
+function sigma(x::Vector,p::Matrix,α,β,κ)
+    n = length(x)
+    σ = zeros(n,2n+1)
+    wm = zeros(2n+1)
+    wc = zeros(2n+1)
+    λ = α^2*(n+κ)-n
+    σ[:,1] = x
+    wm[1] = λ/(n+λ)
+    wc[1] = wm[1] + (1-α^2+β)
+    wm[2:end] = 1/(2*(n+λ))
+    wc[2:end] = wm[2:end]
+    sp = sqrt(n+λ)*full(chol(p))
+    for i = 2:n+1
+        σ[:,i] = x + sp[:,i-1]
+        σ[:,i+n] = x - sp[:,i-1]
+    end
+    (σ,wm,wc)
+end
+
 function sigma(s::UnscentedState)
     n = length(s.x)
     σ = zeros(n,2n+1)
