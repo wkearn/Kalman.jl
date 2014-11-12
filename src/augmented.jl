f45f4097e62a13c9f6f0bbc2045cc511b63196f4
Will Kearney <wkearn@gmail.com>
2014-11-12 12:47:31 -0500

    Remove extraneous sigma point calculations
    Those are now done appropriately in unscented.jl

--- src/augmented.jl
+++ src/augmented.jl
@@ -80,58 +80,7 @@ function covs(kf::AugmentedUnscentedKalmanFilter,y::Observation)
     return res,ph,s
 end
 
-function sigma(x::Vector,p::Matrix,α,β,κ)
-    n = length(x)
-    σ = zeros(n,2n+1)
-    wm = zeros(2n+1)
-    wc = zeros(2n+1)
-    λ = α^2*(n+κ)-n
-    σ[:,1] = x
-    wm[1] = λ/(n+λ)
-    wc[1] = wm[1] + (1-α^2+β)
-    wm[2:end] = 1/(2*(n+λ))
-    wc[2:end] = wm[2:end]
-    sp = sqrt(n+λ)*full(chol(p))
-    for i = 2:n+1
-        σ[:,i] = x + sp[:,i-1]
-        σ[:,i+n] = x - sp[:,i-1]
-    end
-    (σ,wm,wc)
-end
-
-function sigma(s::AugmentedUnscentedState)
-    n = length(s.x)
-    σ = zeros(n,2n+1)
-    wm = zeros(2n+1)
-    wc = zeros(2n+1)
-    λ = s.α^2*(n+s.κ)-n
-    σ[:,1] = s.x
-    wm[1] = λ/(n+λ)
-    wc[1] = wm[1] + (1-s.α^2+s.β)
-    wm[2:end] = 1/(2*(n+λ))
-    wc[2:end] = wm[2:end]
-    sp = sqrt(n+λ)*full(chol(s.p))
-    for i = 2:n+1
-        σ[:,i] = s.x + sp[:,i-1]
-        σ[:,i+n] = s.x - sp[:,i-1]
-    end
-    (σ,wm,wc)
-end
-
-function sigmaweights(n,α,β,κ)
-    wm = zeros(2n+1)
-    wc = zeros(2n+1)
-    λ = α^2*(n+κ)-n
-    wm[1] = λ/(n+λ)
-    wc[1] = wm[1] + (1-α^2+β)
-    for i = 2:2n+1
-        wm[i] = 1/(2*(n+λ))
-        wc[i] = wm[i]
-    end
-    (wm,wc)
-end
-
-function augment(s::AugmentedUnscentedState,R::Matrix)
+function augment(s::UnscentedState,R::Matrix)
     n = length(s.x)
     m = size(R,1)
     x = [s.x,zeros(m)]
