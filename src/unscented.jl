ce04e27a6f7b2dd4dca5d03a2529b5e62a377612
Will Kearney <wkearn@gmail.com>
2014-11-14 17:12:36 -0500

    Move all the additive unscented Kalman filter stuff into one file

--- src/unscented.jl
+++ src/unscented.jl
@@ -1,8 +1,29 @@
-include("unscentedtypes.jl")
+#######
+# Unscented Kalman filter types and filtering interface
 
-# Predict and update functions
+abstract UnscentedKalmanFilter <: KalmanFilter
+abstract AbstractUnscentedState <: AbstractState
 
-# Now a target for abstraction
+type UnscentedState{T} <: AbstractUnscentedState
+    x::Vector{T}
+    p::Matrix
+    α::Real
+    β::Real
+    κ::Real
+end
+
+type AdditiveUnscentedObservationModel <: ObservationModel
+    h::Function
+    r::Matrix
+end
+
+type AdditiveUnscentedModel <: Model
+    f::Function
+    q::Matrix
+end
+
+## ap function for AdditiveUnscentedModels and UnscentedStates
+
 function ap(f::AdditiveUnscentedModel,s::UnscentedState)
     σn,wm,wc = sigma(s)
     σs = zeros(σn)
@@ -18,6 +39,20 @@ function ap(f::AdditiveUnscentedModel,s::UnscentedState)
     pn += f.q
     UnscentedState(xn,pn,s.α,s.β,s.κ)
 end
+
+type AdditiveUnscentedKalmanFilter <: UnscentedKalmanFilter
+    x::UnscentedState
+    f::AdditiveUnscentedModel
+    z::AdditiveUnscentedObservationModel
+end
+
+
+function AdditiveUnscentedKalmanFilter(x::Vector,p::Matrix,f::AdditiveUnscentedModel,z::AdditiveUnscentedObservationModel,α::Real,β::Real,κ::Real)
+    s = UnscentedState(x,p,α,β,κ)
+    AdditiveUnscentedKalmanFilter(s,f,z)
+end
+
+### covs function for additive unscented Kalman filters
 
 function covs(kf::AdditiveUnscentedKalmanFilter,y::Observation)
     n = size(kf.x.x,1)
@@ -51,4 +86,43 @@ function covs(kf::AdditiveUnscentedKalmanFilter,y::Observation)
     s += kf.z.r
     return res,ph,s
 end
+#######
+# Sigma point generating functions
 
+function sigma(x::Vector,p::Matrix,α,β,κ)
+    n = length(x)
+    σ = zeros(n,2n+1)
+    wm = zeros(2n+1)
+    wc = zeros(2n+1)
+    λ = α^2*(n+κ)-n
+    σ[:,1] = x
+    wm[1] = λ/(n+λ)
+    wc[1] = wm[1] + (1-α^2+β)
+    wm[2:end] = 1/(2*(n+λ))
+    wc[2:end] = wm[2:end]
+    sp = sqrt(n+λ)*full(chol(p))
+    for i = 2:n+1
+        σ[:,i] = x + sp[:,i-1]
+        σ[:,i+n] = x - sp[:,i-1]
+    end
+    (σ,wm,wc)
+end
+
+function sigma(s::UnscentedState)
+    n = length(s.x)
+    σ = zeros(n,2n+1)
+    wm = zeros(2n+1)
+    wc = zeros(2n+1)
+    λ = s.α^2*(n+s.κ)-n
+    σ[:,1] = s.x
+    wm[1] = λ/(n+λ)
+    wc[1] = wm[1] + (1-s.α^2+s.β)
+    wm[2:end] = 1/(2*(n+λ))
+    wc[2:end] = wm[2:end]
+    sp = sqrt(n+λ)*full(chol(s.p))
+    for i = 2:n+1
+        σ[:,i] = s.x + sp[:,i-1]
+        σ[:,i+n] = s.x - sp[:,i-1]
+    end
+    (σ,wm,wc)
+end
