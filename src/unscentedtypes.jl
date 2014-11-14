365f9ce1e3acc7a99d8f5eed4f16e37a41c0d06c
Will Kearney <wkearn@gmail.com>
2014-11-14 17:07:48 -0500

    Remove old-style sigma point methods

--- src/unscentedtypes.jl
+++ src/unscentedtypes.jl
@@ -76,35 +76,3 @@ function sigma(s::UnscentedState)
     end
     (σ,wm,wc)
 end
-
-function sigma(x::Vector,p::Matrix,α,κ)
-    n = length(x)
-    σ = fill(zeros(x),2n+1)
-    λ = sqrt(n+α^2*(n+κ)-n)
-    σ[1] = x
-    sp = λ*full(chol(p))
-    for i = 2:n+1
-        σ[i] = x + sp[:,i-1]
-        σ[i+n] = x - sp[:,i-1]
-    end
-    σ
-end
-
-sigma(x::State,α,κ) = sigma(x.x,x.p,α,κ)
-
-function sigma(kf::AdditiveUnscentedKalmanFilter; recalc=true)
-    recalc ? sigma(kf.x,kf.α,kf.κ) : kf.σ
-end
-
-function sigmaweights(n,α,β,κ)
-    wm = zeros(2n+1)
-    wc = zeros(2n+1)
-    λ = α^2*(n+κ)-n
-    wm[1] = λ/(n+λ)
-    wc[1] = wm[1] + (1-α^2+β)
-    for i = 2:2n+1
-        wm[i] = 1/(2*(n+λ))
-        wc[i] = wm[i]
-    end
-    (wm,wc)
-end
